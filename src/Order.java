25ab21571b95fc569df762e928530014a6802463
RomanMartynyshyn <ivancapperi@gmail.com>
2025-03-13 10:53:30 +0200

    add order

--- src/Order.java
+++ src/Order.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Order {
+    String itemName;
+    String managerName;
+    double prise;
+    String city;
+    String customer;
+
+    public Order(String itemName, String managerName, double prise, String city, String customer) {
+        this.itemName = itemName;
+        this.managerName = managerName;
+        this.prise = prise;
+        this.city = city;
+        this.customer = customer;
+    }
 }
